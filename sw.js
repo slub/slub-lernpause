7d70de52021ed884028b690559176bb1375fe6b2
Arne Ruemmler <arne.ruemmler@gmail.com>
2025-01-22 10:50:09 +0100

    initial commit

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'random-quote-pwa';
+const assetsToCache = [
+    './',
+    './index.html',
+    './data.csv',
+    './manifest.json'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(assetsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
